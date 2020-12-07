0b580d3a72c950f523a55cab7fd0050a540b4317
Wesley <wesley.contato@hotmail.com>
2020-12-07 06:41:58 -0300

    Projeto inicial

---
+++ ByteBank.Forum/ByteBank.Forum/ViewModels/ContaRegistrarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class ContaRegistrarViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Nome comppleto")]
+        public string NomeCompleto { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+
+    }
+}
